206adbee233e2981cf99e8ab8135fc49a490f3c7
Danzel1473 <kamrimue1996@gmail.com>
2024-06-27 07:38:48 +0900

    0.13

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,17 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Control;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
-namespace RPG.SceneManagement {
+namespace RPG.SceneManagement
+{
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier{
+            A,B,C,D,E
+        }
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeInTime = 1f;
+        [SerializeField] float fadeOutTime = 1f;
+        [SerializeField] float fadeWaitTime = 0.5f;
+
+
+
         public void OnTriggerEnter(Collider other){
             if(other.tag == "Player") {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
         }
+        
+        private IEnumerator Transition()
+        {
+            if(sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set");
+                yield break;
+            }
+
+            Fader fader = FindObjectOfType<Fader>();
+            DontDestroyOnLoad(gameObject);
+            yield return fader.FadeOut(fadeOutTime);
+
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
+
+
+            Destroy(gameObject);
+
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>()){
+                if(portal == this) continue;
+                if(portal.destination != destination) continue;
+
+                return portal;
+            }
+            return null;
+
+        }
     }
+
 }
 
